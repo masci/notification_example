55f5837384af9e416669c582ef10e5d22a3b84e2
Massimiliano Pippi <masci@evonove.it>
2013-10-13 15:23:09 +0200

    user login

--- notification/views.py
+++ notification/views.py
@@ -1,15 +1,24 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
 from django.contrib.messages import add_message
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.core.urlresolvers import reverse
 
 import stored_messages
 
 
-class IndexView(TemplateView):
+class IndexView(FormView):
     template_name = 'notification/homepage.html'
+    form_class = AuthenticationForm
+    success_url = '/'
 
     def get_context_data(self, **kwargs):
         add_message(self.request, stored_messages.STORED_INFO, 'You visited the homepage')
         return super(IndexView, self).get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        login(self.request, form.get_user())
+        return super(IndexView, self).form_valid(form)
 
 
 class MessagesView(TemplateView):
