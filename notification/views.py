b9a3f3e5b5ee3ba90c83cdd1f84e3e16dc9f0021
Massimiliano Pippi <masci@evonove.it>
2013-10-13 14:21:58 +0200

    using stored_messages, cleanup some pyc files

--- notification/views.py
+++ notification/views.py
@@ -1,9 +1,21 @@
 from django.views.generic import TemplateView
+from django.contrib.messages import add_message
+
+import stored_messages
 
 
 class IndexView(TemplateView):
     template_name = 'notification/homepage.html'
 
+    def get_context_data(self, **kwargs):
+        add_message(self.request, stored_messages.STORED_INFO, 'You visited the homepage')
+        return super(IndexView, self).get_context_data(**kwargs)
+
 
 class MessagesView(TemplateView):
     template_name = 'notification/messages.html'
+
+    def get_context_data(self, **kwargs):
+        add_message(self.request, stored_messages.STORED_INFO,
+                    'You visited the notifications page')
+        return super(MessagesView, self).get_context_data(**kwargs)
