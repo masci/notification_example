bb4749337803148a8e50a3dfb4bf9840b3902981
Massimiliano Pippi <masci@evonove.it>
2013-10-19 11:54:26 +0200

    urls for rest api

--- notification_example/urls.py
+++ notification_example/urls.py
@@ -8,12 +8,6 @@ from notification.views import IndexView, MessagesView
 
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view(), name='home'),
-    url(r'^messages', MessagesView.as_view(), name='messages'),
-    # url(r'^notification_example/', include('notification_example.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^messages/$', MessagesView.as_view(), name='messages'),
+    url(r'^api/', include('stored_messages.urls')),
 )
