ab3a63d5f32a701511effaccada88bfa03543798
Massimiliano Pippi <masci@evonove.it>
2013-10-19 16:30:42 +0200

    first working angular app

--- notification/static/notification/notification.js
+++ notification/static/notification/notification.js
@@ -1,34 +1,19 @@
 var messageApp = angular.module('messageApp', []);
 
 
-messageApp.controller('MainCtrl', ['$scope', function ($scope) {
+messageApp.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
     // Messages array
-    $scope.inbox = {};
+    $scope.messages = {};
 
     // retrieve Messages from the restAPI
     $http({
         method: 'GET',
-        url: '//localhost:8000/api/inbox'
+        url: '//127.0.0.1:8000/api/inbox/'
     })
     .success(function (data, status, headers, config) {
-        $scope.inbox.messages = data;
+        $scope.messages = data;
     })
     .error(function (data, status, headers, config) {
         // something went wrong :(
     });
 }]);
-
-
-messageApp.directive('readitButton', function () {
-  return {
-    restrict: 'A',
-    replace: true,
-    transclude: true,
-    template: '<a href="" class="myawesomebutton" ng-transclude>' +
-                '<i class="icon-ok-sign"></i>' +
-              '</a>',
-    link: function (scope, element, attrs) {
-      // DOM manipulation/events here!
-    }
-  };
-});
