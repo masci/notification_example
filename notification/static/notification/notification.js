aec976d146c79c1cfee02861b7097b916d4bd170
Massimiliano Pippi <masci@evonove.it>
2013-10-20 10:59:52 +0200

    mark single messages as read

--- notification/static/notification/notification.js
+++ notification/static/notification/notification.js
@@ -16,4 +16,21 @@ messageApp.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
     .error(function (data, status, headers, config) {
         // something went wrong :(
     });
+
+    // mark messages read
+    $scope.markRead = function (index) {
+        var id = $scope.messages[index].message.id;
+        $http({
+            method: 'POST',
+            url: '//127.0.0.1:8000/api/inbox/'+id+'/read/',
+            xsrfHeaderName: 'X-CSRFToken',
+            xsrfCookieName: 'csrftoken'
+        })
+        .success(function (data, status, headers, config) {
+            $scope.messages.splice(index, 1)
+        })
+        .error(function (data, status, headers, config) {
+            // something went wrong :(
+        })
+    };
 }]);
