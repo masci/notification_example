e87924dec18a044ac3a8c85961daecb1122b6a8b
Massimiliano Pippi <masci@evonove.it>
2013-10-21 19:04:24 +0200

    added logout button

--- notification_example/urls.py
+++ notification_example/urls.py
@@ -4,6 +4,7 @@ from notification.views import IndexView, MessagesView
 
 
 urlpatterns = patterns('',
+    url(r'^logout/$', 'django.contrib.auth.views.logout',  {'next_page': '/'}, name='logout'),
     url(r'^$', IndexView.as_view(), name='home'),
     url(r'^messages/$', MessagesView.as_view(), name='messages'),
     url(r'^api/', include('stored_messages.urls')),
