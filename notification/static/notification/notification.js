50e7be8d8eca21290c3de8eadd2058a25dad3e86
Bobo <fabrizio@moldiscovery.com>
2014-03-12 10:22:26 +0100

    added broadcast message test

--- notification/static/notification/notification.js
+++ notification/static/notification/notification.js
@@ -49,4 +49,24 @@ messageApp.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
             // something went wrong :(
         })
     };
+    // send a Broadcast Message
+    $scope.sendBroadcast = function () {
+        $http({
+            method: 'GET',
+            url: '//127.0.0.1:8000/broadcast/',
+        })
+        .success(function(data, status, headers, config) {
+	    // retrieve Messages from the restAPI
+	    $http({
+		method: 'GET',
+		url: '//127.0.0.1:8000/api/inbox/'
+	    })
+	    .success(function (data, status, headers, config) {
+		$scope.messages = data;
+	    })
+        })
+        .error(function(data, status, headers, config){
+            // something went wrong :(
+        })
+    };
 }]);
--- notification/views.py
+++ notification/views.py
@@ -2,7 +2,10 @@ from django.views.generic import TemplateView, FormView
 from django.contrib.messages import add_message
 from django.contrib.auth import login
 from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponse
 
+
+from stored_messages import broadcast_message
 import stored_messages
 
 
@@ -19,6 +22,12 @@ class IndexView(FormView):
         login(self.request, form.get_user())
         return super(IndexView, self).form_valid(form)
 
+
+def BroadcastView(request):
+    if request.method == 'GET':
+        broadcast_message( stored_messages.STORED_INFO, 'This is a broadcast message from user: '+str(request.user) )
+        return HttpResponse('Ok')
+     
 
 class MessagesView(TemplateView):
     template_name = 'notification/messages.html'
